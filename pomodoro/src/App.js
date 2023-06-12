f43ad4f5d8e1cecdaaad0e10a7b5b95c3d25bef6
Cyndaquil1999 <mcah8278@yahoo.co.jp>
2023-06-12 09:39:43 +0900

    UIの変更

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -4,7 +4,7 @@ import Timer from './modules/Timer';
 const App = () => {
   return (
     <div>
-      <Timer workTime={25} breakTime={5} longBreakTime={15} cycles={4} />
+      <Timer />
     </div>
   );
 };
--- pomodoro/src/modules/Timer.jsx
+++ pomodoro/src/modules/Timer.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/Timer.css';
+
 
 const Timer = () => {
   const [workTime, setWorkTime] = useState(25);
@@ -101,59 +103,91 @@ const Timer = () => {
 
   return (
     <div className="container">
-      <h1>Pomodoro Timer</h1>
-      <h2>{formatTime(seconds)}</h2>
-      <Form>
-        <Form.Group controlId="workTime">
-          <Form.Label>Work Time (minutes): {workTime}</Form.Label>
-          <Form.Control
-            type="range"
-            min={1}
-            max={60}
-            value={workTime}
-            onChange={handleWorkTimeChange}
-          />
-        </Form.Group>
+      <Row>
+        <Col>
+          <Row className='show-times'>
+            <Col>
+              <div>Work Time</div>
+              <h1>{formatTime(workTime * 60)}</h1>
+            </Col>
+            <Col>
+              <div>Break Time</div>
+              <h1>{formatTime(breakTime * 60)}</h1>
+            </Col>
+            <Col>
+              <div>Long Break Time</div>
+              <h1>{formatTime(longBreakTime * 60)}</h1>
+            </Col>
+          </Row>
+          <Row className='show-cycles'>
+            <Col>
+              <div>Pomodoro Cycles</div>
+              <h1>{cycles}</h1>
+            </Col>
+            <Col>
+              <div>Completed Cycles</div>
+              <h1>{completedCycles}</h1>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group controlId="workTime">
+              <Form.Label>Work Time (minutes)</Form.Label>
+              <Form.Control
+                type="range"
+                min={1}
+                max={60}
+                value={workTime}
+                onChange={handleWorkTimeChange}
+              />
+            </Form.Group>
 
-        <Form.Group controlId="breakTime">
-          <Form.Label>Break Time (minutes): {breakTime}</Form.Label>
-          <Form.Control
-            type="range"
-            min={1}
-            max={60}
-            value={breakTime}
-            onChange={handleBreakTimeChange}
-          />
-        </Form.Group>
+            <Form.Group controlId="breakTime">
+              <Form.Label>Break Time (minutes)</Form.Label>
+              <Form.Control
+                type="range"
+                min={1}
+                max={60}
+                value={breakTime}
+                onChange={handleBreakTimeChange}
+              />
+            </Form.Group>
 
-        <Form.Group controlId="longBreakTime">
-          <Form.Label>Long Break Time (minutes): {longBreakTime}</Form.Label>
-          <Form.Control
-            type="range"
-            min={1}
-            max={60}
-            value={longBreakTime}
-            onChange={handleLongBreakTimeChange}
-          />
-        </Form.Group>
+            <Form.Group controlId="longBreakTime">
+              <Form.Label>Long Break Time (minutes)</Form.Label>
+              <Form.Control
+                type="range"
+                min={1}
+                max={60}
+                value={longBreakTime}
+                onChange={handleLongBreakTimeChange}
+              />
+            </Form.Group>
 
-        <Form.Group controlId="cycles">
-          <Form.Label>Cycles: {cycles}</Form.Label>
-          <Form.Control
-            type="range"
-            min={1}
-            max={10}
-            value={cycles}
-            onChange={handleCyclesChange}
-          />
-        </Form.Group>
-
-        <Button onClick={handleStart}>Start</Button>
-        <Button onClick={handlePause}>Pause</Button>
-        <Button onClick={handleReset}>Reset</Button>
-
-        <p>Completed Cycles: {completedCycles}</p>
-      </Form>
+            <Form.Group controlId="cycles">
+              <Form.Label>Cycles</Form.Label>
+              <Form.Control
+                type="range"
+                min={1}
+                max={10}
+                value={cycles}
+                onChange={handleCyclesChange}
+              />
+            </Form.Group>
+            <br />
+            <div className="button-container d-flex justify-content-between gap-3">
+              <Button onClick={handleStart} style={{width: '300px'}} className='start-button'>Start</Button>
+              <Button onClick={handlePause} style={{width: '300px'}} className='pause-button'>Pause</Button>
+              <Button onClick={handleReset} style={{width: '300px'}} className='reset-button'>Reset</Button>
+            </div>
+            
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 };
